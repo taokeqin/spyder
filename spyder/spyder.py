21a91f0dd4b4e21a93e8a72c7d88e25276324f4c
taokeqin <taokeqin@gmail.com>
2015-08-04 22:03:51 +0800

    refine basic page download

--- spyder/spyder.py
+++ spyder/spyder.py
@@ -3,17 +3,28 @@ import argparse
 import hashlib
 from urlparse import urlparse
 
-optparser = argparse.ArgumentParser(description='spyder application.')
-optparser.add_argument('-u', '--url', dest='url', help='base url')
-args = optparser.parse_args()
-if not args.url:
-    optparser.print_help()
-    quit() 
+class Spyder:
 
-r = requests.get(args.url)
-urlp = urlparse(args.url)
-hostname = urlp.netloc
-pathname = urlp.path
-urlhash = hashlib.sha224(hostname+'/'+pathname).hexdigest()
-with open(urlhash, 'w') as f:
-    f.write(r.content)
+    def __init__(self):
+	self._handle_input()
+
+    def _handle_input(self):
+        optparser = argparse.ArgumentParser(description='spyder application.')
+        optparser.add_argument('-u', '--url', dest='url', help='base url')
+        self.args = optparser.parse_args()
+        if not self.args.url:
+            optparser.print_help()
+            quit() 
+
+    def start(self):
+        self.r = requests.get(self.args.url)
+        urlp = urlparse(self.args.url)
+        hostname = urlp.netloc
+        pathname = urlp.path
+        urlhash = hashlib.sha224(hostname+'/'+pathname).hexdigest()
+        with open(urlhash, 'w') as f:
+            f.write(self.r.content)
+
+if __name__ == "__main__":
+    s = Spyder()
+    s.start()
